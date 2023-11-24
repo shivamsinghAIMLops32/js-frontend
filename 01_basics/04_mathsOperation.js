8979f4e8df76ea9b692ffe45c242f414837ac223
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-11-24 15:54:42 +0000

    basic operations

---
+++ 01_basics/04_mathsOperation.js
@@ -0,0 +1,12 @@
+/*
+basic math operation
+{Add, Sub, Mult, Power, Divide, Modulus(for reminder)}
+ */
+
+console.log(3+4);
+console.log(9-2);
+console.log(3*8);    // multiply by using asterik (*)
+console.log(16/4);   // divide by using (/)
+console.log(20%10);  // reminder 0 cause (% is modulus operator)
+console.log(10**2);  // power of any number use double asterik (**)
+
