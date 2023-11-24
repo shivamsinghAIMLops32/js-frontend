178d5e7e485c69104808da266dbddf1922ff26e6
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-11-24 14:39:26 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all JS code as newer version.
+
+// alert(3-2);   we are using node JS not browser.
+
+let name ='shivam';
+let age = 21;
+let isLoggedIn = false;
+let temperature = null;
+let state;
+
+// Number => 2 to power 53
+// BigInt => bigger size integer
+// String => ""
+// Boolean => true/false
+// Null => standalone value
+// Undefined => not defined yet may be defined later on
+// Symbol => uniqueness of component
+
+
+// Object 
+
+console.log(temperature)
+// console.table(typeof [name, age, isLoggedIn, temperature, state]);
+
+console.log(typeof null);       // it is object type.
+console.log(typeof undefined);  // it is of undefined type
