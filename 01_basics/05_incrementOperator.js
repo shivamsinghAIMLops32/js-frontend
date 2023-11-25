0bb4c355f353336846cfd40036eeae5f68013910
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-11-25 09:02:43 +0000

    increment file push

---
+++ 01_basics/05_incrementOperator.js
@@ -0,0 +1,18 @@
+/*
+let x = 3;
+x++;
+console.log(x);
+
+let y = 5;
+++y;
+console.log(y);
+*/
+
+let x = 3;
+const y = x++;
+console.log(`x: ${x}, y: ${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+
