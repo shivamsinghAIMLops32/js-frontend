c42defc132e67154dda40bf9a436375a32aa4711
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-11-25 09:19:38 +0000

    conversion file push

---
+++ 01_basics/06_comparisonOperation.js
@@ -0,0 +1,26 @@
+// console.log(3<4);
+// console.log(3<=4);
+// console.log(3<4);
+// console.log(3<=4);
+// console.log(5<2);
+// console.log(10==10);
+// console.log(10===10);
+// console.log(6===10);
+// console.log(6!=10);
+
+/* console.log("2" >=1);     // self changed to Number and returned true.
+console.log(2 >="1"); */
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0);
+
+// undefined always give false.
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Strict Check by using triple equal to (===).
+console.log("2" === 2);   // triple equal to wont let change data type from string to NUMBER so It comparison wont take place.
+console.log("2" == 2);    // double equal to will let change string to number type and return TRUE.
