380e02630fc754d8084c7729b8e8ee5acbe18976
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-11-25 08:43:55 +0000

    maths opertaion push

--- 01_basics/04_mathsOperation.js
+++ 01_basics/04_mathsOperation.js
@@ -3,10 +3,39 @@ basic math operation
 {Add, Sub, Mult, Power, Divide, Modulus(for reminder)}
  */
 
+/*
 console.log(3+4);
 console.log(9-2);
 console.log(3*8);    // multiply by using asterik (*)
 console.log(16/4);   // divide by using (/)
 console.log(20%10);  // reminder 0 cause (% is modulus operator)
 console.log(10**2);  // power of any number use double asterik (**)
+*/
 
+/*
+let str1 ="hello";
+let str2 = " shivam";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("2" + 3);
+console.log(2 + "3");
+console.log("1" +2 +3);   // treats everyone as strings
+console.log(1 + "2" + 3);
+console.log(1 + 2 + "3");  // if string is at last so first num is Number so it'll be taken as number first then will be string given
+*/
+
+
+console.log(+true);
+//console.log(true+);
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 5;
+console.table([num1,  num2, num3]);
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
